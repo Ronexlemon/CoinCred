d03f7b15a6b843adb0ceb6575b853c0aed3d07c1
RonexLemon <ronexondimu@gmail.com>
2024-05-19 13:32:16 +0300

    feat:navbar

--- client/coincred/src/components/Narvbar.tsx
+++ client/coincred/src/components/Narvbar.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ConnectButton from "./connect";
+import { Button } from "./ui/button";
+import { SiVault } from "react-icons/si";
+import { GiLiquidSoap } from "react-icons/gi";
+import { BsBank } from "react-icons/bs";
+import { GiTakeMyMoney } from "react-icons/gi";
 
 const Narvbar =()=>{
     return(
-        <main className="h-40 w-full flex flex-col bg-backGroundColor fixed  ">
+        <main className="h-40 w-full flex flex-col bg-backGroundColor fixed  gap-8 ">
             <div className="w-full flex justify-between items-center" >
                 <h1 className="text-textColor">COINCREDD</h1>
                 <div className=" rounded-xl border-2 border-textColor">
@@ -16,6 +21,10 @@ const Narvbar =()=>{
 
             </div>
             <div className="w-full flex justify-evenly  items-center bg-cardBackgroud rounded-xl" >
+<Button className="flex gap-2 justify-between items-center" variant="default"><SiVault/> Smart Vaults</Button>
+<Button className="flex gap-2 justify-between items-center" variant="default"><GiLiquidSoap /> Liquidate</Button>
+<Button className="flex gap-2 justify-between items-center" variant="default"><BsBank/> Requests</Button>
+<Button className="flex gap-2 justify-between items-center" variant="default"><GiTakeMyMoney/> Loans Issued</Button>
 
 
 
--- client/coincred/src/components/ui/button.tsx
+++ client/coincred/src/components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: " text-textColor hover:bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
@@ -18,6 +18,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        accent: "bg-accentColor text-textColor rounded-xl"
       },
       size: {
         default: "h-10 px-4 py-2",
