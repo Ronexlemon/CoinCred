b7fdca0f92455bd97073a4117173f06fecf51d1c
RonexLemon <ronexondimu@gmail.com>
2024-05-23 13:23:10 +0300

    feat:navigation

--- client/coincred/src/components/Narvbar.tsx
+++ client/coincred/src/components/Narvbar.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from "react";
 import ConnectButton from "./connect";
 import { Button } from "./ui/button";
@@ -5,8 +7,10 @@ import { SiVault } from "react-icons/si";
 import { GiLiquidSoap } from "react-icons/gi";
 import { BsBank } from "react-icons/bs";
 import { GiTakeMyMoney } from "react-icons/gi";
+import { useRouter } from "next/navigation";
 
 const Narvbar =()=>{
+    const router = useRouter();
     return(
         <main className="h-40 w-full flex flex-col bg-backGroundColor   gap-8  ">
             <div className="w-full flex justify-between items-center" >
@@ -21,10 +25,10 @@ const Narvbar =()=>{
 
             </div>
             <div className="w-full flex justify-evenly  items-center bg-cardBackgroud rounded-xl" >
-<Button className="flex gap-2 justify-between items-center" variant="default"><SiVault/> Smart Vaults</Button>
-<Button className="flex gap-2 justify-between items-center" variant="default"><GiLiquidSoap /> Liquidate</Button>
-<Button className="flex gap-2 justify-between items-center" variant="default"><BsBank/> Requests</Button>
-<Button className="flex gap-2 justify-between items-center" variant="default"><GiTakeMyMoney/> Loans Issued</Button>
+<Button onClick={()=> router.replace("/")} className="flex gap-2 justify-between items-center" variant="default"><SiVault/> Smart Vaults</Button>
+<Button onClick={()=> router.replace("/liquidate")} className="flex gap-2 justify-between items-center" variant="default"><GiLiquidSoap /> Liquidate</Button>
+<Button onClick={()=> router.replace("/requests")} className="flex gap-2 justify-between items-center" variant="default"><BsBank/> Requests</Button>
+<Button onClick={()=> router.replace("/loanissue")} className="flex gap-2 justify-between items-center" variant="default"><GiTakeMyMoney/> Loans Issued</Button>
 
 
 
