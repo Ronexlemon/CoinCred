f6fd1a95ca473436a4e4b960967ca47681eab3bc
RonexLemon <ronexondimu@gmail.com>
2024-05-25 16:45:16 +0300

    request Page

---
+++ client/coincred/src/app/request/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import React from "react";
+import LoanRequests from "@/components/LoanRequests";
+
+export default function Home() {
+  return (
+    <main className=" h-screen  ">
+      <LoanRequests/>
+      
+    </main>
+  );
+}
