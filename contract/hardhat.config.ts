109b52787ef99e4024dc4b934cbd81e4e5d36e0c
RonexLemon <ronexondimu@gmail.com>
2024-05-25 14:46:30 +0300

    chore :deploy contract

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,9 +1,41 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+require("dotenv").config({path:".env"});
+const KEY = process.env.PRIVATE_KEY as string;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks:{
+    liskSepolia :{
+      url: `https://rpc.sepolia-api.lisk.com`,  
+      chainId: 4202,
+      accounts: [KEY],
+      
+    },
+    
+  },
+  etherscan: {
+    // Use "123" as a placeholder, because Blockscout doesn't need a real API key, and Hardhat will complain if this property isn't set.
+     apiKey: {
+      "lisk-sepolia": "123"
+     },
+     customChains: [
+      {
+          network: "lisk-sepolia",
+          chainId: 4202,
+          urls: {
+              apiURL: "https://sepolia-blockscout.lisk.com/api",
+              browserURL: "https://sepolia-blockscout.lisk.com"
+          }
+       }
+     ]
+   },
+   sourcify: {
+    // Disabled by default
+    // Doesn't need an API key
+    enabled: true
+  }
 };
 
 export default config;
---
+++ contract/ignition/modules/CoinCred.ts
@@ -0,0 +1,13 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+
+
+const LockModule = buildModule("CoinCred", (m) => {
+    
+ 
+  const coincred = m.contract("CoinCred",[['0x19Ea0584D2A73265251Bf8dC0Bc5A47DebF539ac'],['0xF75D4Bb568c93C1e88690B98B54814ACFE349ED5'],['0x4200000000000000000000000000000000000006'],['0x07DCD270159185725346361CB8462d36b3dAb90F']] );
+
+  return { coincred };
+});
+
+export default LockModule;
